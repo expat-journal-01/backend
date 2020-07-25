85a1ad3ce0559f0f5f6fb57bb651a6d741d73e8c
hera <a.heraimenka@gmail.com>
2020-07-25 10:02:20 -0700

    Add tests for register

---
+++ api/auth/authRouter.spec.js
@@ -0,0 +1,71 @@
+const request = require("supertest");
+const db = require("../../data/dbConfig");
+const server = require("../../server");
+
+
+describe("Test register", () => {
+    beforeAll(async () => {
+        await db("user").truncate();
+    });
+    
+
+    afterAll(async () => {
+        await db("user").truncate();
+    });
+
+
+
+    test("Create a user successfully", async () => {
+        const response = await request(server)
+            .post("/api/auth/register")
+            .send({
+                username: "Jamie",
+                password: "1234"
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body).toHaveLength(1);
+        expect(response.body[0]["id"]).toBeTruthy();
+    });
+
+
+    test("Can't create a user with the same username and password", async () => {
+        const response = await request(server)
+            .post("/api/auth/register")
+            .send({
+                username: "Jamie",
+                password: "1234"
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body.error).toBeTruthy();
+    });
+
+
+    test("Can't create a user with empty username and password", async () => {
+        const response = await request(server)
+            .post("/api/auth/register")
+            .send({
+                username: "",
+                password: ""
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body.error).toBeTruthy();
+    });
+
+
+    test("Can't create a user with missing data", async () => {
+        const response = await request(server)
+            .post("/api/auth/register")
+            .send({});
+
+        expect(response.status).toBe(400);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body.error).toBeTruthy();
+    });
+
+});
---
+++ knexfile.js
@@ -0,0 +1,73 @@
+// Update with your config settings.
+
+module.exports = {
+    development: {
+        client: "pg",
+        connection: {
+            database: process.env.DB_NAME,
+            user:     process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations",
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
+    },
+    testing: {
+        client: "pg",
+        connection: {
+            database: process.env.DB_NAME_TESTING,
+            user:     process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations",
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
+    },
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user:         'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user:         'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    }
+
+};
