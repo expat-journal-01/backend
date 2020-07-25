1d6a1c339eb80c4073c00b4e2950d49d1b934c6f
hera <a.heraimenka@gmail.com>
2020-07-25 10:25:42 -0700

    Log in

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -27,9 +27,39 @@ router.post("/register", checkUsernameUnique, (req, res) => {
 
 
 router.post("/login", (req, res) => {
-    res.status(501).json({
-        error: "Not implemented"
-    });
+    userDb.getByUsername(req.body.username)
+        .then(users => {
+
+            if (
+                users.length &&
+                bcrypt.compareSync(req.body.password, users[0]["password"])
+            ) {
+                const token = jwt.sign(
+                    {
+                        id: users[0]["id"],
+                        username: users[0]["username"]
+                    },
+                    process.env.SECRET,
+                    {
+                        expiresIn: "1d"
+                    }
+                );
+
+                res.status(200).json({
+                    token
+                });
+            } else {
+                res.status(403).json({
+                    error: "Wrong username or password.",
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Server error. Could not log in.",
+                description: error
+            });
+        });
 });
 
 
