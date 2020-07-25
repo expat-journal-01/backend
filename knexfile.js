5b408be313a5e1b3c5676d522b26f21158dc9eab
hera <a.heraimenka@gmail.com>
2020-07-25 11:06:46 -0700

    Update connection settings for heroku

--- knexfile.js
+++ knexfile.js
@@ -39,34 +39,19 @@ module.exports = {
             directory: "./data/seeds"
         }
     },
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user:         'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
-    },
     production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user:         'username',
-            password: 'password'
-        },
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
             max: 10
         },
         migrations: {
-            tableName: 'knex_migrations'
+            tableName: "knex_migrations",
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
         }
     }
 
