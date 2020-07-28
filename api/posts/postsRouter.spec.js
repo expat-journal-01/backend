aa9eca3d2866bc1125cdec8af76a527dfa7b1d23
hera <a.heraimenka@gmail.com>
2020-07-27 18:40:52 -0700

    Add more tests for posts

--- api/posts/postsRouter.spec.js
+++ api/posts/postsRouter.spec.js
@@ -1,8 +1,24 @@
 const request = require("supertest");
+const fs = require("fs");
+const path = require("path");
 
-const db = require("../../data/dbConfig");
 const server = require("../../server");
 const { cleanDb } = require("../../data/helpers");
+
+
+async function removeAllUploads () {
+    const directory = "uploads";
+
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+}
 
 
 let authToken;
@@ -10,6 +26,7 @@ let authToken;
 describe("Stories", () => {
     beforeAll(async () => {
         await cleanDb();
+        await removeAllUploads();
 
         // Create a user
         await request(server)
@@ -47,6 +64,7 @@ describe("Stories", () => {
 
     afterAll(async () => {
         await cleanDb();
+        await removeAllUploads();
     });
 
 
@@ -77,7 +95,7 @@ describe("Stories", () => {
             .field("title", "Sample title")
             .field("description", "Sample description")
             .field("storyId", 1)
-            .attach("image", "api/posts/test_images/small.jpg")
+            .attach("image", "api/posts/test/small.jpg")
             .set("Authorization", authToken)
             .then((response) => {
                 const expected = [
@@ -97,8 +115,67 @@ describe("Stories", () => {
 
                 done();
             });
+    });
 
-        
+    test("Can upload images only", async (done) => {
+        request(server)
+            .post("/api/posts")
+            .field("title", "Sample title")
+            .field("description", "Sample description")
+            .field("storyId", 1)
+            .attach("image", "api/posts/test/dummy.txt")
+            .set("Authorization", authToken)
+            .then((response) => {
+                
+                expect(response.status).toBe(400);
+                expect(response.headers["content-type"]).toMatch(/application\/json/);
+                expect(response.body.error).toBeTruthy();
+
+                done();
+            });
+    });
+
+
+    test("Get all posts. 1 present", async () => {
+        const response = await request(server)
+            .get("/api/posts")
+            .set("Authorization", authToken);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body).toHaveLength(1);
+    });
+
+
+    test("Get a post by id", async () => {
+        const response = await request(server)
+            .get("/api/posts/1")
+            .set("Authorization", authToken);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body).toHaveLength(1);
+    });
+
+
+    test("Get a post by userId", async () => {
+        const response = await request(server)
+            .get("/api/posts/user/1")
+            .set("Authorization", authToken);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body).toHaveLength(1);
+    });
+
+    test("Get a post by storyId", async () => {
+        const response = await request(server)
+            .get("/api/posts/story/1")
+            .set("Authorization", authToken);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toMatch(/application\/json/);
+        expect(response.body).toHaveLength(1);
     });
 
 });
